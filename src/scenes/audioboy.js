2263cc3dd3fa941ee26f3d71529770c50b2ed9bb
Phildo <phildo211@gmail.com>
2018-07-23 13:27:55 -0500

    can control audio

---
+++ src/scenes/audioboy.js
@@ -0,0 +1,57 @@
+var audioboy = function()
+{
+  var self = this;
+  var twopi = 3.14159265*2.0;
+  self.buff_s = 0.05;
+  var buff_channels = 2;
+  self.filled_playhead_s = 0;
+  self.filled_playhead_gap_s = 0;
+
+  var playing = false;
+
+  var actx = new window.AudioContext();
+  var buff = actx.createBuffer(buff_channels, actx.sampleRate*self.buff_s, actx.sampleRate);
+  var source;
+
+  self.vol = 1;
+  self.freq = 440;
+
+  var fill = function()
+  {
+    if(!playing)
+    {
+      source = null;
+      return;
+    }
+    source = actx.createBufferSource()
+    for(var channel = 0; channel < buff.numberOfChannels; channel++)
+    {
+      var data = buff.getChannelData(channel);
+      for(var i = 0; i < buff.length; i++)
+      {
+        var x = ((i/buff.length*self.buff_s)+self.filled_playhead_s)*twopi*self.freq;
+        var y = Math.sin(x);
+        data[i] = y*self.vol;
+      }
+    }
+    self.filled_playhead_s += self.buff_s+self.filled_playhead_gap_s;
+
+    source.buffer = buff;
+    source.connect(actx.destination);
+    source.loop = 0;
+    source.onended = fill;
+    source.start();
+  }
+  fill();
+
+  self.play = function()
+  {
+    playing = true;
+    if(!source) fill();
+  }
+  self.stop = function()
+  {
+    playing = false;
+  }
+}
+
--- src/scenes/gameplayScene.js
+++ src/scenes/gameplayScene.js
@@ -11,23 +11,67 @@ var GamePlayScene = function(game, stage)
     canv = stage.canv;
     canvas = canv.canvas;
     ctx = canv.context;
+
+    if(clicker) { clicker.detach(); clicker = new Clicker({source:stage.canv.canvas}); };
+    if(dragger) { dragger.detach(); dragger = new Dragger({source:stage.canv.canvas}); };
   }
   self.resize(stage);
 
+  var clicker;
+  var dragger;
+  var play_btn;
+  var freq_slider;
+  var vol_slider;
+  var ab;
+
   self.ready = function()
   {
+    clicker = new Clicker({source:stage.canv.canvas});
+    dragger = new Dragger({source:stage.canv.canvas});
+    ab = new audioboy();
+    //ab.play();
+
+    var x = 10;
+    var y = 10;
+    var w; var h;
+    w = 100;
+    h = 100;
+    play_btn = new ToggleBox(x,y,w,h,0,function(v){ if(v) ab.play(); else ab.stop(); });
+    y += h;
+    y += 10;
+    w = 200;
+    h = 20;
+    freq_slider = new SmoothSliderBox(x,y,w,h,20,20000,440,function(v){ ab.freq = v; });
+    y += h;
+    y += 10;
+    w = 200;
+    h = 20;
+    vol_slider = new SmoothSliderBox(x,y,w,h,0,1,1,function(v){ ab.vol = v; });
   };
 
   self.tick = function()
   {
+    clicker.filter(play_btn);
+    dragger.filter(freq_slider);
+    dragger.filter(vol_slider);
+    clicker.flush();
+    dragger.flush();
+
+    freq_slider.tick();
+    vol_slider.tick();
   };
 
   self.draw = function()
   {
+    play_btn.draw(canv);
+    freq_slider.draw(canv);
+    vol_slider.draw(canv);
   };
 
   self.cleanup = function()
   {
+    clicker.detach(); clicker = null;
+    dragger.detach(); dragger = null;
   };
 
 };
